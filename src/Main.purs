29199b8e637ebc4fe1c6dbf4abe01bd20d104cff
Howard Chen <hchen7913@gmail.com>
2025-12-26 21:04:17 -0600

    Write out stub for the script

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,28 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+{- Ensures that the given method in the module is run as the 'main' function on NodeJs. This will enable shell commands like 
+
+`webb-exec Main build`
+-}
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  pure unit
+  {-}
+  shell <- Shell.new
+  Shell.buildProject
+  options <- Options.new
+  moduleName <- Options.moduleName options
+  methodName <- Options.methodName options 
+  paths <- Paths.new
+  mod <- Paths.findModule paths moduleName methodName
+  dir <- Dir.new
+  Dir.ensureOutputDir dir mod
+  Mod.writeCallingFile mod dir
+  outputFile <- Dir.outputFilePath dir
+  Shell.execute shell outputFile
+  -}
+  
+  
 
